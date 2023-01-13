9412486d5d785109e785bc3cbc63044fe2d065fc
Vanilla <osu_Vanilla@126.com>
2023-01-13 16:50:31 +0800

    feat[system/users.nix]: Remove user password for easy login.

--- system/users.nix
+++ system/users.nix
@@ -6,9 +6,7 @@
     isNormalUser = true;
     extraGroups = [ "wheel" ];
 
-    hashedPassword = "$6" + "$NixOS/Jovian"
-      + "$KqCR5fNyd1XhsqlRrkl6TqVd4zOgVPaTuyF"
-      + "/aCXzVT1LFypV1slG.p46JnxQgBCJZ4Ecz5gCUyREqrcRCC.0z0";
+    password = "";
   };
 
   users.mutableUsers = false;
