56e556d21eb9d1e3c90df764548e0de915d60b4d
Vanilla <osu_Vanilla@126.com>
2023-01-13 15:14:14 +0800

    fix[services/xserver.nix]: Join nm groups for connection in setup.

--- services/xserver.nix
+++ services/xserver.nix
@@ -2,6 +2,7 @@
 {
   jovian.steam.enable = true;
   networking.networkmanager.enable = true;
+  users.extraUsers."vanilla".extraGroups = [ "networkmanager" ];
 
   services.xserver.enable = true;
   services.xserver.displayManager.autoLogin.enable = true;
