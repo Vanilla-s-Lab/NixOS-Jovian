c76e5c046e4849107a3a0e6abe5c497dcb83c961
Vanilla <osu_Vanilla@126.com>
2023-01-14 21:16:38 +0800

    fix[services/openssh.nix]: vanilla -> root, for deploy-rs.

--- services/openssh.nix
+++ services/openssh.nix
@@ -3,7 +3,7 @@
   services.openssh.enable = true;
   services.openssh.passwordAuthentication = false;
 
-  users.users."vanilla".openssh.authorizedKeys.keyFiles =
+  users.users."root".openssh.authorizedKeys.keyFiles =
     lib.singleton (pkgs.fetchurl {
       url = "https://github.com/VergeDX.keys"; # Yubikeys 5 NFC
       hash = "sha256-ASglsi7Ape5I9MabvIiWXPI7IeumBaoVTb+pX3Hm5EA=";
