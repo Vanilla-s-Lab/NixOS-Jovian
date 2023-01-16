d4398410cd98f678aea28f36e243533a2a417d2b
Vanilla <osu_Vanilla@126.com>
2023-01-16 15:22:04 +0800

    fix[services/xserver.nix]: Also apply Win10 fonts for steam.

--- services/xserver.nix
+++ services/xserver.nix
@@ -9,6 +9,19 @@
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.mobile.enable = true;
 
-  nixpkgs.overlays = [ nur.overlay ];
-  environment.systemPackages = [ pkgs.nur.repos.vanilla.Win10_LTSC_2021_fonts ];
+  nixpkgs.overlays = [
+    nur.overlay
+
+    (self: super: {
+      steam = super.steam.override {
+        extraPkgs = pkgs: [
+          pkgs.nur.repos.vanilla.Win10_LTSC_2021_fonts
+        ];
+      };
+    })
+  ];
+
+  environment.systemPackages = [
+    pkgs.nur.repos.vanilla.Win10_LTSC_2021_fonts
+  ];
 }
